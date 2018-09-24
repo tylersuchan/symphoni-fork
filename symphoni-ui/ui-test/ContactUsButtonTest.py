fa9159142a5675a8322bf2c55783010e5b2f6c4d
unknown <tylersuc@buffalo.edu>
2018-09-23 23:48:38 -0400

    Updated test suite to match easier identification as updated in index.html file

--- symphoni-ui/ui-test/ContactUsButtonTest.py
+++ symphoni-ui/ui-test/ContactUsButtonTest.py
@@ -1,65 +1,42 @@
 import unittest
+from os import path, getcwd
+from driver_picker import DriverPicker
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class PrivacyButtonTest(unittest.TestCase):
+class ContactUsButtonTest(unittest.TestCase):
+        def setUp(self):
+            self.GoogleDriver = webdriver.Chrome(DriverPicker().driver_version)
+            self.GoogleDriver.get("file://" + path.join(getcwd(), 'public', 'index.html'))
 
-    def setUp(self):
-        self.GoogleDriver = webdriver.Chrome()
-        self.FirefoxDriver = webdriver.Firefox()
-        self.location = "file:///home/ross/CSE442/symphoni/symphoni-ui/public/index.html"
+        def test_contact_us_page_source(self):
+            driver = self.GoogleDriver
+            assert "Contact Us" in driver.page_source
+            
+        def test_contact_us_button_display(self):
+            driver = self.GoogleDriver
+            assert driver.find_element_by_id("contactus-button").is_displayed()
 
-    def test1(self):
-        driver = self.GoogleDriver
-        driver.get(self.location)
-        assert "Privacy Policy" in driver.page_source
-
-    def test2(self):
-        driver = self.GoogleDriver
-        driver.get(self.location)
-        driver.implicitly_wait(1)
-        assert driver.find_element_by_xpath("""//*[@id="root"]/h3/a""").is_displayed()
-
-    def test3(self):
-        driver = self.GoogleDriver
-        driver.get(self.location)
-        driver.implicitly_wait(1)
-        assert driver.find_element_by_xpath("""//*[@id="root"]/h3/a""").is_enabled()
-
-    def test4(self):
-        driver = self.FirefoxDriver
-        driver.get(self.location)
-        driver.implicitly_wait(1)
-
-        element = driver.find_element_by_xpath("""//*[@id="root"]/h3/a""").click()
-        driver.implicitly_wait(1)
-        assert '#privacy' in driver.current_url
-
-    def test5(self):
-        driver = self.GoogleDriver
-        driver.get(self.location)
-        driver.implicitly_wait(1)
-        element = driver.find_element_by_xpath("""//*[@id="root"]/h3/a""").click()
-        driver.implicitly_wait(1)
-        assert '#privacy' in driver.current_url
-
-    def test6(self):
-        driver = self.GoogleDriver
-        driver.get(self.location)
-        driver.implicitly_wait(1)
-        assert driver.find_element_by_id("privacy").is_displayed()
-
-    def test7(self):
-        driver = self.GoogleDriver
-        driver.get(self.location)
-        driver.implicitly_wait(1)
-        assert driver.find_element_by_id("privacy").is_enabled()
-
-    def tearDown(self):
-        self.GoogleDriver.close()
-        self.FirefoxDriver.close()
+        def test_contact_us_button_enabled(self):
+            driver = self.GoogleDriver
+            assert driver.find_element_by_id("contactus-button").is_enabled()
 
 
-if __name__ == "__main__":
-    unittest.main()
+        def test_contact_us_button_link(self):
+            driver = self.GoogleDriver
+            element = driver.find_element_by_id("contactus-button").click()
+            driver.implicitly_wait(1)
+            assert "#contactus" in driver.current_url
+
+        def test_contact_us_display(self):
+            driver = self.GoogleDriver
+            assert driver.find_element_by_id("contactus").is_displayed()
+
+        def test_contact_us_enabled(self):
+            driver = self.GoogleDriver
+            assert driver.find_element_by_id("contactus").is_enabled()
+
+        def tearDown(self):
+            self.GoogleDriver.close()
+
