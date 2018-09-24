0aed6676087120d6c77fd6afe2df3369fd66b444
Ross Ventresca <rossvent@buffalo.edu>
2018-09-24 00:09:11 -0400

    Integrated contact us button tests with rest of the test suite

--- symphoni-ui/Tests/test_suite.py
+++ symphoni-ui/Tests/test_suite.py
@@ -5,6 +5,7 @@ import start_party_button
 import queue
 import privacy_button
 import music_queue
+import contact_button
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
@@ -15,5 +16,6 @@ suite.addTests(loader.loadTestsFromModule(start_party_button))
 suite.addTests(loader.loadTestsFromModule(queue))
 suite.addTests(loader.loadTestsFromModule(privacy_button))
 suite.addTests(loader.loadTestsFromModule(music_queue))
+suite.addTests(loader.loadTestsFromModule(contact_button))
 
 runner.run(suite)
