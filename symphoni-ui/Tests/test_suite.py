b441b835fad18d6da2995ed4d9f60bad747f5abb
Ross Ventresca <rossvent@buffalo.edu>
2018-09-23 23:15:29 -0400

    Integrated privacy policy tests with rest of test suite

--- symphoni-ui/Tests/test_suite.py
+++ symphoni-ui/Tests/test_suite.py
@@ -3,6 +3,7 @@ import unittest
 import join_button
 import start_party_button
 import queue
+import privacy_button
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
@@ -11,5 +12,6 @@ runner = unittest.TextTestRunner(verbosity=3)
 suite.addTests(loader.loadTestsFromModule(join_button))
 suite.addTests(loader.loadTestsFromModule(start_party_button))
 suite.addTests(loader.loadTestsFromModule(queue))
+suite.addTests(loader.loadTestsFromModule(privacy_button))
 
 runner.run(suite)
