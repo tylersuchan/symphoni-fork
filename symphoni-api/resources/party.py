24ae9635d29b3e36029b52e0e91233589dca8f8f
Ross Ventresca <rossvent@buffalo.edu>
2018-10-19 16:20:38 -0400

    Implemeted playlist field in party data

--- symphoni-api/resources/party.py
+++ symphoni-api/resources/party.py
@@ -6,20 +6,23 @@ import persistence
 
 class Party(Resource):
     def get(self, name):
-        if not name in persistence.db:
+        code = name
+        if not code in persistence.db:
             return 404
-        retval = {'code': name, 'name': persistence.db[name]} 
+        retval = {'code': code, 'party_data': persistence.db[code]} 
         return retval,200 
 
     def put(self, name):
         code = ''.join(random.choices(
             string.ascii_uppercase + string.digits, k=6))
         retval = {'code': code, 'name': name}
-        persistence.db[code] = name
+        persistence.db[code] = { 'name': name, 'playlist' : [] }
+        retval = {'code': code, 'party_data': persistence.db[code]}
         return retval, 201
 
     def post(self,name):
-        if not name in persistence.db:
+        code = name
+        if not code in persistence.db:
             return 404
         
         parser = reqparse.RequestParser()
@@ -32,17 +35,18 @@ class Party(Resource):
             return {"message": "Invalid Input"},400
 
 
-        persistence.db[name] = args['name']
-        retval = {'code': name, 'name': persistence.db[name]}
+        persistence.db[code]['name'] = args['name']
+        retval = {'code': code, 'party_data': persistence.db[code]}
 
         return retval,201
 
     def delete(self,name): 
-        if not name in persistence.db:
+        code = name
+        if not code in persistence.db:
             return 404
         
         try:
-            del persistence.db[name]
+            del persistence.db[code]
         except:
             return 500
         
