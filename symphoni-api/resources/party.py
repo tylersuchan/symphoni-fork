fb694847e837fa2be847d4f2f836ca217ce61229
Ross Ventresca <rossvent@buffalo.edu>
2018-10-20 13:23:18 -0400

    Implemented creation of the party via a spotify access token

--- symphoni-api/resources/party.py
+++ symphoni-api/resources/party.py
@@ -2,7 +2,7 @@ from flask_restful import reqparse, Resource
 import random
 import string
 import persistence
-
+import json
 
 class Party(Resource):
     def get(self, name):
@@ -15,17 +15,21 @@ class Party(Resource):
     def put(self, name):
 
         parser = reqparse.RequestParser()
-        parser.add_argument('access_token')
+        parser.add_argument('access_token',type=str, location='json', required=True)
+        parser.add_argument('token_type',type=str, location='json', required=True)
+        parser.add_argument('expires_in',type=int, location='json', required=True)
+        parser.add_argument('refresh_token',type=str, location='json', required=True)
+        parser.add_argument('scope',type=str, location='json', required=True)
+        parser.add_argument('expires_at',type=int, location='json', required=True)
         try:
             args = parser.parse_args(strict=True)
-            if args['access_token'] is None or args['access_token'] is "":
-                raise Exception('Input is None') 
         except:
             return {"message": "Invalid Input"},400
 
         code = ''.join(random.choices(
             string.ascii_uppercase + string.digits, k=6))
-        persistence.db[code] = { 'name': name, 'playlist' : [], 'access_token': args['access_token'] }
+
+        persistence.db[code] = { 'name': name, 'playlist' : [], 'oauth_token': args}
         retval = {'code': code, 'party_data': persistence.db[code]}
         return retval, 201
 
