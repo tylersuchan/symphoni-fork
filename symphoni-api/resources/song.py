d66a34f548db4b9f8efc25bb1aa6bc56fcd8ee51
tylersuchan <tylersuc@buffalo.edu>
2018-10-21 12:46:59 -0400

    Implemented GET CRUD operation for new Song Resource class. This class deals with retreiving a variety from information from spotify, including uris, artist/track/album info, and other information that can be used for front end development.

--- symphoni-api/resources/song.py
+++ symphoni-api/resources/song.py
@@ -6,44 +6,55 @@ import string
 import persistence
 import json
 
+
 class Song(Resource):
     def get(self, code):
         if not code in persistence.db:
             return 404
-        
+
         parser = reqparse.RequestParser()
-        parser.add_argument('track',type=str)
-        parser.add_argument('artist',type=str)
-        parser.add_argument('album',type=str)
+        parser.add_argument("track", type=str)
+        parser.add_argument("artist", type=str)
+        parser.add_argument("album", type=str)
         try:
             args = parser.parse_args(strict=True)
         except:
-            return {"message": "Invalid Input"},400
+            return {"message": "Invalid Input"}, 400
 
+        spotify = spotipy.Spotify(
+            auth=persistence.db[code]["oauth_token"]["access_token"]
+        )
 
-        spotify = spotipy.Spotify(auth=persistence.db[code]['oauth_token']['access_token'])
+        jsonResult = spotify.search(q="track:" + args["track"], type="track")
+        retval = {"results": []}
 
-        return spotify.search(q='track:'+args['track'],type='track')
-                
-        return retval,200 
+        for items in jsonResult["tracks"]["items"]:
 
-    def put(self, name):
+            artists = []
 
-        parser = reqparse.RequestParser()
-        parser.add_argument('access_token',type=str, location='json', required=True)
-        parser.add_argument('token_type',type=str, location='json', required=True)
-        parser.add_argument('expires_in',type=int, location='json', required=True)
-        parser.add_argument('refresh_token',type=str, location='json', required=True)
-        parser.add_argument('scope',type=str, location='json', required=True)
-        parser.add_argument('expires_at',type=int, location='json', required=True)
-        try:
-            args = parser.parse_args(strict=True)
-        except:
-            return {"message": "Invalid Input"},400
+            for artist in items["artists"]:
 
-        code = ''.join(random.choices(
-            string.ascii_uppercase + string.digits, k=6))
+                artist_json = {
+                    "artist_name": artist["name"],
+                    "artist_uri": artist["uri"]
+                }
 
-        persistence.db[code] = { 'name': name, 'playlist' : [], 'oauth_token': args}
-        retval = {'code': code, 'party_data': persistence.db[code]}
-        return retval, 201
+                artists.append(artist_json)
+
+            album_json = {
+                "album_name": items["album"]["name"],
+                "album_images": items["album"]["images"],
+                "album_release_date": items["album"]["release_date"],
+                "album_uri": items["album"]["uri"]
+            }
+
+            song_json = {
+                "track": items["name"],
+                "track_uri": items["uri"],
+                "album_information": album_json,
+                "artist_information": artists
+            }
+
+            retval["results"].append(song_json)
+
+        return retval, 200
