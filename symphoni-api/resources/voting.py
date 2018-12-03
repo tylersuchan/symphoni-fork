98e3ae7d575ad81e6c6b2403676aa67a5a14c501
Corwyn Nielsen <corwynnielsen@Corwyns-MacBook-Pro.local>
2018-12-03 01:21:10 -0500

    Added basic username inputs for use in upvoting and downvoting functionality

--- symphoni-api/resources/voting.py
+++ symphoni-api/resources/voting.py
@@ -4,6 +4,7 @@ import string
 import persistence
 import json
 
+
 class Voting(Resource):
 
     def put(self, code):
@@ -12,17 +13,18 @@ class Voting(Resource):
             abort(404, message="Code {} doesn't exist".format(code))
 
         parser = reqparse.RequestParser()
-        parser.add_argument('track_uri',type=str,location='args',required=True)
-        parser.add_argument('vote',type=str,location='args',required=True)
-        parser.add_argument('user',type=str,location='args',required=True)
+        parser.add_argument('track_uri', type=str,
+                            location='args', required=True)
+        parser.add_argument('vote', type=str, location='args', required=True)
+        parser.add_argument('user', type=str, location='args', required=True)
 
         try:
             args = parser.parse_args(strict=True)
             if not(("up" in args["vote"]) or ("down" in args["vote"])):
                 raise Exception("Please specify vote (up or down)")
-            if(args['track_uri'] is None or args['track_uri'] is ""):
+            if args['track_uri'] is None or args['track_uri'] is "":
                 raise Exception('Please specify a valid track_uri')
-            if(args['user'] is None or args['user'] is ""):
+            if args['user'] is None or args['user'] is "":
                 raise Exception('Please enter a valid user in the party')
         except:
             abort(400, message="Invalid arguments")
@@ -30,18 +32,19 @@ class Voting(Resource):
         songFound = False
         userFound = False
 
-        if args['user'] in persistence.db[code]["users"]: #Checks if user is in party
+        if args['user'] in persistence.db[code]["users"]:  # Checks if user is in party
             userFound = True
         else:
             abort(400, message="The requested user is not in the party")
 
         for item in persistence.db[code]["playlist"]:
             if item["song"]["track_uri"] == args["track_uri"]:
-                if(userFound):
-                    if(args['user'] in item['user_votes']):
+                if userFound:
+                    if args['user'] in item['user_votes']:
                         abort(400, message="A user can only vote on song once")
                     else:
-                        item["vote"] = item["vote"]+1 if "up" in args["vote"] else item["vote"]-1
+                        item["vote"] = item["vote"] + \
+                            1 if "up" in args["vote"] else item["vote"]-1
                         #item["user_votes"].append({args['user']:"up"} if "up" in args["vote"] else {args['user']:"down"})
                         item["user_votes"].append(args['user'])
                         songFound = True
@@ -49,9 +52,10 @@ class Voting(Resource):
                 else:
                     abort(404, message="Only users in the party can vote on songs")
 
-        if(not songFound):
+        if not songFound:
             abort(404, message="Song not found in the playlist")
-        newlist = sorted(persistence.db[code]["playlist"],key=lambda k: k["vote"], reverse=True)
+        newlist = sorted(
+            persistence.db[code]["playlist"], key=lambda k: k["vote"], reverse=True)
         persistence.db[code]["playlist"] = newlist
         retval = {'code': code, 'party_data': persistence.db[code]}
-        return retval,201
+        return retval, 201
--- symphoni-ui/src/components/StartPartyContainer/index.jsx
+++ symphoni-ui/src/components/StartPartyContainer/index.jsx
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Container from '../Util/Container';
+import UsernameInput from '../Util/UsernameInput';
 import config from '../../config';
 
 class StartPartyContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.partyCode = undefined;
+    this.state = { partyCreated: false };
+  }
+
   getPartyCodeOnEnter = (event) => {
     if (event.key === 'Enter') {
       const partyURI = `${config.url}party/${event.target.value}`;
@@ -12,30 +19,40 @@ class StartPartyContainer extends Component {
       }).then(response => response.json().then((data) => {
         const { setPartyCode, setPartyName, isHost } = this.props;
         if (response.ok) {
-          setPartyCode(data.code);
+          this.partyCode = data.code;
+          setPartyCode(this.partyCode);
           setPartyName(data.party_data.name);
           isHost();
           window.Materialize.toast('Party created successfully!', 4000);
+          this.setState({ partyCreated: true });
         }
       }));
     }
   };
 
   render() {
+    const { partyCreated } = this.state;
+    const { setUsername } = this.props;
+
     return (
       <Container id="start" className="page-header">
-        <div className="center" id="start-party">
-          <h3> Start Party Here!</h3>
-          <h5> Insert Party Name</h5>
-        </div>
-        <div className="flex-container">
-          <div className="flex-horizontal-center input-field col s6">
-            <i className="material-icons prefix">music_note</i>
-            {' '}
-            <input id="join-code" type="text" onKeyPress={this.getPartyCodeOnEnter} />
-            <label htmlFor="join-code">Party Name</label>
-          </div>
-        </div>
+        {!partyCreated && (
+          <Fragment>
+            <div className="center" id="start-party">
+              <h3> Start Party Here!</h3>
+              <h5> Insert Party Name</h5>
+            </div>
+            <div className="flex-container">
+              <div className="flex-horizontal-center input-field col s6">
+                <i className="material-icons prefix">music_note</i>
+                {' '}
+                <input id="join-code" type="text" onKeyPress={this.getPartyCodeOnEnter} />
+                <label htmlFor="join-code">Party Name</label>
+              </div>
+            </div>
+          </Fragment>
+        )}
+        {partyCreated && <UsernameInput setUsername={setUsername} partyCode={this.partyCode} />}
       </Container>
     );
   }
@@ -45,6 +62,7 @@ StartPartyContainer.propTypes = {
   setPartyCode: PropTypes.func.isRequired,
   setPartyName: PropTypes.func.isRequired,
   isHost: PropTypes.func.isRequired,
+  setUsername: PropTypes.func.isRequired,
 };
 
 export default StartPartyContainer;
