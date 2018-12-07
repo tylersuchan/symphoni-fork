eae69eb7b1a21bc1451700c879a245349d041f9b
Corwyn Nielsen <corwynnielsen@Corwyns-MacBook-Pro.local>
2018-12-06 23:35:30 -0500

    Added get to voting endpoint and changed how the returned data is formatted. Also retreive current voting data on reload.

--- symphoni-api/resources/voting.py
+++ symphoni-api/resources/voting.py
@@ -6,6 +6,7 @@ import json
 
 
 class Voting(Resource):
+
     def get(self, code):
         if not code in persistence.db:
             abort(404, message="Code {} doesn't exist".format(code))
@@ -13,21 +14,30 @@ class Voting(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('track_uri', type=str,
                             location='args', required=True)
-        parser.add_argument('user', type=str, location='args', required=True)
 
         try:
             args = parser.parse_args(strict=True)
             if args['track_uri'] is None or args['track_uri'] is '':
                 raise Exception('Please specify a valid track_uri')
-            if args['user'] is None or args['user'] is '':
-                raise Exception('Please enter a valid user in the party')
         except:
             abort(400, message='Invalid arguments')
 
         party = persistence.db[code]
+        track_uri = args['track_uri']
 
-        if user in ['users']:
-            pass
+        matching_song = next((song for song in party['playlist']
+                              if song['song']['track_uri'] == track_uri), None)
+        if matching_song:
+            retval = {
+                'message': 'Voting data retreived for {}'.format(matching_song['song']['track']),
+                'code': code,
+                'vote_data': {
+                    'upvotes': matching_song['upvotes'],
+                    'downvotes': matching_song['downvotes'],
+                    'votes': matching_song['votes'],
+                }
+            }
+            return retval, 200
 
     def put(self, code):
 
@@ -55,40 +65,40 @@ class Voting(Resource):
         user = args['user']
         vote = args['vote']
         track_uri = args['track_uri']
-        voted_song = None
+        matching_song = next((song for song in party['playlist']
+                              if song['song']['track_uri'] == track_uri), None)
 
-        if user in party['users']:
-            for item in party['playlist']:
-                if item['song']['track_uri'] == track_uri:
-                    voted_song = item
-                    if vote == 'up':
-                        if user in item['upvotes']:
-                            item['upvotes'].remove(user)
-                        elif user in item['downvotes']:
-                            item['downvotes'].remove(user)
-                            item['upvotes'].append(user)
-                        else:
-                            item['upvotes'].append(user)
-                    elif vote == 'down':
-                        if user in item['downvotes']:
-                            item['downvotes'].remove(user)
-                        elif user in item['upvotes']:
-                            item['upvotes'].remove(user)
-                            item['downvotes'].append(user)
-                        else:
-                            item['downvotes'].append(user)
-                sortedPlaylist = sorted(
-                    party['playlist'], key=lambda k: k['votes'], reverse=True)
-                item['votes'] = len(item['upvotes']) - len(item['downvotes'])
+        if user in party['users'] and matching_song:
+            if vote == 'up':
+                if user in matching_song['upvotes']:
+                    matching_song['upvotes'].remove(user)
+                elif user in matching_song['downvotes']:
+                    matching_song['downvotes'].remove(user)
+                    matching_song['upvotes'].append(user)
+                else:
+                    matching_song['upvotes'].append(user)
+            elif vote == 'down':
+                if user in matching_song['downvotes']:
+                    matching_song['downvotes'].remove(user)
+                elif user in matching_song['upvotes']:
+                    matching_song['upvotes'].remove(user)
+                    matching_song['downvotes'].append(user)
+                else:
+                    matching_song['downvotes'].append(user)
+
+        sortedPlaylist = sorted(
+            party['playlist'], key=lambda k: k['votes'], reverse=True)
+        matching_song['votes'] = len(
+            matching_song['upvotes']) - len(matching_song['downvotes'])
 
         persistence.db[code]['playlist'] = sortedPlaylist
         retval = {
             'message': "Vote {} for user {} added successfuly.".format(vote, user),
             'code': code,
             'vote_data': {
-                'upvotes': voted_song['upvotes'],
-                'downvotes': voted_song['downvotes'],
-                'votes': voted_song['votes']
+                'upvotes': matching_song['upvotes'],
+                'downvotes': matching_song['downvotes'],
+                'votes': matching_song['votes']
             }
         }
         return retval, 200
--- symphoni-ui/src/components/QueueContainer/VotingButtons.jsx
+++ symphoni-ui/src/components/QueueContainer/VotingButtons.jsx
@@ -1,42 +1,49 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import style from './QueueContainer.css';
 import config from '../../config';
 
 class VotingButtons extends Component {
   constructor(props) {
     super(props);
-    this.votes = [];
-    this.state = { voteStatus: [], voteCount: 0 };
+    this.state = { voteStatus: '', voteCount: 0 };
   }
 
   componentWillMount() {
     this.getVoteData();
   }
 
-  getVoteData = () => {};
+  decideVoteStatus = (upvotes, downvotes) => {
+    const { username } = this.props;
 
-  shrinkVotes = () => {
-    const votesLength = this.votes.length;
-    if (votesLength >= 2 && this.votes[votesLength - 2] === this.votes[votesLength - 1]) {
-      this.votes = [];
-    }
-  };
+    const userInUpvotes = upvotes.includes(username);
+    const userInDownvotes = downvotes.includes(username);
 
-  decideVoteStatus = () => {
-    const currentVote = this.votes[this.votes.length - 1];
-    const prevVote = this.votes[this.votes.length - 2];
-
-    this.shrinkVotes();
-
-    if (currentVote === prevVote) {
-      return '';
-    }
-
-    if (currentVote === 'up') {
+    if (userInUpvotes) {
       return 'up';
     }
 
-    return 'down';
+    if (userInDownvotes) {
+      return 'down';
+    }
+
+    return '';
+  };
+
+  getVoteData = () => {
+    const { partyCode, trackURI } = this.props;
+    const url = new URL(`${config.url}party/${partyCode}/vote`);
+    url.search = new URLSearchParams({ track_uri: trackURI });
+
+    fetch(url, {
+      method: 'GET',
+    }).then(response => response.json().then((data) => {
+      if (response.ok) {
+        const voteData = data.vote_data;
+        const newVoteStatus = this.decideVoteStatus(voteData.upvotes, voteData.downvotes);
+        this.setState({ voteStatus: newVoteStatus, voteCount: voteData.votes });
+      }
+    }));
   };
 
   registerVote = (vote) => {
@@ -48,9 +55,9 @@ class VotingButtons extends Component {
       method: 'PUT',
     }).then(response => response.json().then((data) => {
       if (response.ok) {
-        this.votes.push(vote);
-        const newVoteStatus = this.decideVoteStatus();
-        this.setState({ voteStatus: newVoteStatus, voteCount: data.vote_data.votes });
+        const voteData = data.vote_data;
+        const newVoteStatus = this.decideVoteStatus(voteData.upvotes, voteData.downvotes);
+        this.setState({ voteStatus: newVoteStatus, voteCount: voteData.votes });
       }
     }));
   };
@@ -96,5 +103,11 @@ class VotingButtons extends Component {
     );
   }
 }
+
+VotingButtons.propTypes = {
+  partyCode: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  trackURI: PropTypes.string.isRequired,
+};
 
 export default VotingButtons;
